ff7c413c6b523bf474e464a6e3f864fe9d236e76
Kotaro Sugawara <kotarella1110@gmail.com>
2018-07-12 17:59:55 +0900

    Fixed components

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -12,7 +12,8 @@ class App extends React.Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          To get started, edit <code>src/App/index.tsx</code> and save to
+          reload.
         </p>
       </div>
     );
