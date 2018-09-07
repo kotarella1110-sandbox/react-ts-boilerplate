a70d4a20858d51bbfec4e0796cf317867287b790
Kotaro Sugawara <kotarella1110@gmail.com>
2018-09-07 16:50:39 +0900

    Added styled-components

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,23 +1,56 @@
 import * as React from 'react';
-import './index.css';
+import styled, { keyframes } from 'styled-components';
+import logo from './logo.svg';
 
-import logoSvg from './logo.svg';
+const AppWrapper = styled.div`
+  text-align: center;
+`;
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logoSvg} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App/index.tsx</code> and save to
-          reload.
-        </p>
-      </div>
-    );
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
   }
-}
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const AppLogo = styled.img`
+  animation: ${rotate360} infinite 120s linear;
+  height: 80px;
+  &:hover {
+    animation: ${rotate360} infinite 1.5s linear;
+  }
+`;
+
+const AppHeader = styled.div`
+  background-color: #222;
+  height: 12rem;
+  padding: 1rem;
+  color: white;
+`;
+
+const AppTitle = styled.h1`
+  font-weight: 900;
+`;
+
+const AppIntro = styled.p`
+  font-size: large;
+  code {
+    font-size: 1.3rem;
+  }
+`;
+
+const App: React.SFC<{}> = () => (
+  <AppWrapper>
+    <AppHeader>
+      <AppLogo src={logo} alt="logo" />
+      <AppTitle>Welcome to React</AppTitle>
+    </AppHeader>
+    <AppIntro>
+      To get started, edit <code>src/App/index.tsx</code> and save to reload.
+    </AppIntro>
+  </AppWrapper>
+);
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import { injectGlobal } from 'styled-components';
 import registerServiceWorker from './registerServiceWorker';
+
+injectGlobal`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
+`;
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
 registerServiceWorker();
