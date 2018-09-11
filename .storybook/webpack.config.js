2928adea6cb49619f900a3333656b8f9b0ad2b24
Kotaro Sugawara <kotarella1110@gmail.com>
2018-09-11 17:31:33 +0900

    Fixed webpack config for storybook rewired の変更内容を storybook にも適用

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,16 @@
-const path = require('path');
+process.argv.push('--scripts-version');
+process.argv.push('react-scripts-ts');
+
 const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin');
+const rewires = require('../config-overrides').webpack;
+
 module.exports = (baseConfig, env, config) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    loader: require.resolve('ts-loader')
+    loader: require.resolve('ts-loader'),
+    options: {},
   });
   config.plugins.push(new TSDocgenPlugin()); // optional
   config.resolve.extensions.push('.ts', '.tsx');
-  return config;
+  return rewires(config);
 };
