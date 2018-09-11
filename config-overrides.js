aa2923c696c36a56916eead6201d7f4458172018
Kotaro Sugawara <kotarella1110@gmail.com>
2018-09-11 13:27:03 +0900

    Added tslint-loader

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,26 @@
-const path = require('path');
-const { compose } = require("react-app-rewired");
-const rewireStyledComponents = require("react-app-rewire-styled-components");
+const { compose } = require('react-app-rewired');
+const rewireStyledComponents = require('react-app-rewire-styled-components');
 const rewireStyledComponentsTypescriptPlugin = require('react-app-rewire-styled-components-typescript');
+const paths = require('react-scripts-ts/config/paths');
+
+const rewireTslint = (options = {}) => (config, env) => {
+  const tslintLoader = {
+    test:  /\.(ts|tsx)$/,
+    loader: require.resolve('tslint-loader'),
+    enforce: 'pre',
+    include: paths.appSrc,
+    options: Object.assign({}, options),
+  };
+
+  config.module.rules.unshift(tslintLoader);
+
+  return config;
+};
 
 module.exports = compose(
   rewireStyledComponentsTypescriptPlugin,
-  rewireStyledComponents
+  rewireStyledComponents,
+  rewireTslint({
+    fix: true,
+  }),
 );
